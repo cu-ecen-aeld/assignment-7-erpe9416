9e0e13b46fad63600e30d7900485291523803f7d
hampter <hampter@Ubuntu.myguest.virtualbox.org>
2025-03-05 13:13:10 -0700

    added github username to print statements

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world. This is erpe9416\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world. This is erpe9416\n");
 }
 
 module_init(hello_init);
